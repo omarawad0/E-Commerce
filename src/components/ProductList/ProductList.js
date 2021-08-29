d8c8e242d649807623843abef8f8c2d6761878fe
omarawad0 <ogmal01@gmail.com>
2021-08-29 04:24:56 +0200

    removed capitalizeFirstLetter func and used the build in css text-transform

--- src/components/ProductList/ProductList.js
+++ src/components/ProductList/ProductList.js
@@ -1,19 +1,26 @@
 import React from "react";
 import Product from "../Product/Product";
 import styles from "./ProductList.module.css";
-import { capitalizeFirstLetter } from "../shared/utils/capitalizeFirstLetter";
+import { Link } from "react-router-dom";
+
 class ProductList extends React.Component {
   render() {
     const { categoryName, currency, products } = this.props;
     return (
       <div>
         <div className={styles.categoryName}>
-          <h1>{capitalizeFirstLetter(categoryName)}</h1>
+          <h1>{categoryName}</h1>
         </div>
         <div className={styles.productList}>
           {products.map((product) => {
             return (
-              <Product key={product.id} product={product} currency={currency} />
+              <Link key={product.id} to={`/${product.category}/${product.id}`}>
+                <Product
+                  key={product.id}
+                  product={product}
+                  currency={currency}
+                />
+              </Link>
             );
           })}
         </div>
